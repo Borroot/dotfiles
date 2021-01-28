013b49f13ed286aba7ef2bd88383a024446ef96c
Borroot <borroot@live.nl>
2021-01-28 18:21:13 +0100

    Fix qutebrowser config after update.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,2 +1,5 @@
-c.content.host_blocking.lists.append(str(config.configdir) + "/blocked")
+config.load_autoconfig(False)  # don't load settings configured through the GUI
+
 #c.content.private_browsing = True
+c.content.blocking.method = "both"
+c.content.blocking.hosts.lists.append(str(config.configdir) + "/blocked")
