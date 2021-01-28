f3174dd155c0d04734f676a0d40c9cb0cafcd604
Borroot <borroot@live.nl>
2021-01-28 19:20:21 +0100

    Add qutebrowser shortcuts.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,5 +1,14 @@
 config.load_autoconfig(False)  # don't load settings configured through the GUI
 
+config.bind(',r', 'config-source')  # reload config file
+config.bind(',d', 'download-clear')
+
+config.bind('<Shift-j>', 'tab-prev')
+config.bind('<Shift-k>', 'tab-next')
+
+config.bind('<Shift-Ctrl-j>', 'tab-move -')
+config.bind('<Shift-Ctrl-k>', 'tab-move +')
+
 #c.content.private_browsing = True
-c.content.blocking.method = "both"
-c.content.blocking.hosts.lists.append(str(config.configdir) + "/blocked")
+c.content.blocking.method = 'both'
+c.content.blocking.hosts.lists.append(str(config.configdir) + '/blocked')
