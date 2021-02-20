4319193df1b4080225bcc17f2a4682e0b56e790b
Borroot <borroot@live.nl>
2021-02-20 09:14:57 +0100

    Fix mpv spawn from qutebrowser and add yt channel.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,7 +2,7 @@ config.load_autoconfig(False)  # don't load settings configured through the GUI
 
 config.bind(',r', 'config-source')  # reload config file
 config.bind(',d', 'download-clear')
-config.bind(',m', 'spawn mpv {url}')
+config.bind(',m', 'spawn --detach mpv {url}')
 
 config.bind('<Shift-j>', 'tab-prev')
 config.bind('<Shift-k>', 'tab-next')
@@ -10,6 +10,5 @@ config.bind('<Shift-k>', 'tab-next')
 config.bind('<Shift-Ctrl-j>', 'tab-move -')
 config.bind('<Shift-Ctrl-k>', 'tab-move +')
 
-#c.content.private_browsing = True
 c.content.blocking.method = 'both'
 c.content.blocking.hosts.lists.append(str(config.configdir) + '/blocked')
